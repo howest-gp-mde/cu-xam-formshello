86fe6f0a680f6bf27061ab6bc067f366855c798f
sigged <sderdeyn@gmail.com>
2019-09-02 22:06:42 +0200

    add platform specific code by using DependencyService
    (cherry picked from commit 24679fde2e3b5bdd3e925b40888661a9c59f0a3f)
    (cherry picked from commit af4f101188b2bfef781d66a511f45763280f39ac)

---
+++ XrnCourse.FormsHello/XrnCourse.FormsHello.UWP/Services/MessageServiceUwp.cs
@@ -0,0 +1,13 @@
+﻿//ensures the shared class library has access to this class. Must be defined outside of the namespace
+[assembly: Xamarin.Forms.Dependency(typeof(XrnCourse.FormsHello.UWP.Services.MessageServiceUWP))]
+
+namespace XrnCourse.FormsHello.UWP.Services
+{
+    class MessageServiceUWP : IMessageService
+    {
+        public string GetWelcomeMessage()
+        {
+            return "UWP!";
+        }
+    }
+}
