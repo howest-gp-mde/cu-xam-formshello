9d5df8020f9ab805c28daa9144c8c0e77ed0a2d1
sigged <sderdeyn@gmail.com>
2019-09-03 00:51:11 +0200

    add NavigationPage, AlertPage, Actionsheets and ModalPage
    (cherry picked from commit 05f6f4ca2f6786963b83b141edd9bb1be36a0c83)
    (cherry picked from commit 695440b79a454591d62529909691ae9a88eccb1d)

---
+++ XrnCourse.FormsHello/XrnCourse.FormsHello/AlertPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XrnCourse.FormsHello
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlertPage : ContentPage
+    {
+        public AlertPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnPopupHello_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "Hello there!", "Hi!");
+        }
+
+        private async void BtnGoBack_Clicked(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Goin' back", "Wanna go back?", "Yep", "No, wait!"))
+                await Navigation.PopAsync(true);
+        }
+    }
+}
