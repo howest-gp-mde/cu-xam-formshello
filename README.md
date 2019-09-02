695440b79a454591d62529909691ae9a88eccb1d
sigged <sderdeyn@gmail.com>
2019-09-03 00:51:11 +0200

    add NavigationPage, AlertPage, Actionsheets and ModalPage
    (cherry picked from commit 05f6f4ca2f6786963b83b141edd9bb1be36a0c83)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# xrnforms-formshellopcl
-Showcases basic Xamarin.Forms functionalities using a .NET Standard Class Library as the shared code base.
+# xrnforms-formshello
+Showcases basic Xamarin.Forms functionalities using a .NET Standard Class Library as the shared codebase.
---
+++ XrnCourse.FormsHello/XrnCourse.FormsHello/ModalPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XrnCourse.FormsHello
+{
+    public class ModalPage : ContentPage
+    {
+        public string NameEntered { get; set; }
+        private Entry txtName = new Entry();
+
+        public ModalPage()
+        {
+            //create the UI programmatically
+            Padding = new Thickness(0, 20, 0, 20);
+
+            Button btnClose = new Button { Text = "Submit" };
+            btnClose.Clicked += BtnClose_Clicked;
+
+            StackLayout layout = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Children = {
+                    new Label { Text = "Enter your name:" },
+                    txtName,
+                    btnClose
+                }
+            };
+            Content = layout;
+        }
+
+        private async void BtnClose_Clicked(object sender, EventArgs e)
+        {
+            NameEntered = txtName.Text; //fill property with user input
+            await Navigation.PopModalAsync(true); //close modal
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true; //don't allow back button when this window is opened
+        }
+
+    }
+}
