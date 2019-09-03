1c7f7644ee3281c1df3f4a61990dfa65ee4c2559
sigged <sderdeyn@gmail.com>
2019-09-03 14:04:49 +0200

    add RandomNumberPage and corresponding button on MainPage
    (cherry picked from commit 32158711e9fa3564d85a97763c36e8dda005e61a)
    (cherry picked from commit 3f1503678eae10e6df074742a61a394fab48b6bf)

---
+++ XrnCourse.FormsHello/XrnCourse.FormsHello/RandomNumberPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace XrnCourse.FormsHello
+{
+    public partial class RandomNumberPage : ContentPage
+    {
+        Random rnd = new Random();
+        bool pageVisible = false;
+        int total = 0;
+
+        public RandomNumberPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            pageVisible = true;
+            Device.StartTimer(TimeSpan.FromMilliseconds(1000), () => GenerateNumber());
+        }
+        protected override void OnDisappearing()
+        {
+            pageVisible = false;
+            base.OnDisappearing();
+        }
+
+        private void btnClearTotal_Clicked(object sender, EventArgs e)
+        {
+            total = 0;
+            lblTotal.Text = "-";
+        }
+        private bool GenerateNumber()
+        {
+            int number = rnd.Next(100);
+            Debug.WriteLine($"Last Random Number: {number}");
+            total += number;
+            lblTotal.Text = total.ToString("N0");
+            lblNumber.Text = number.ToString();
+            return pageVisible; //true = repeat
+        }
+
+    }
+
+}
